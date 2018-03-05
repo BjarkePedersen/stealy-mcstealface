0967d47cf4d5c183981c68d7b49f9a240de9153e
oeb25 <oliverboving@gmail.com>
2018-03-05 17:28:16 +0100

    Add better car physics

--- src/misc.ts
+++ src/misc.ts
@@ -12,6 +12,8 @@ export const lerp = ([x0, y0]: Vec2, t: number, [x1, y1]: Vec2): Vec2 => [
 	y0 + t * (y1 - y0),
 ]
 export const len = ([x, y]: Vec2): number => Math.sqrt(x * x + y * y)
+export const abs = ([x, y]: Vec2): Vec2 => [Math.abs(x), Math.abs(y)]
+export const normalize = (a: Vec2): Vec2 => mul(a, 1 / len(a))
 export const unit = (a: Vec2): Vec2 => mul(a, 1 / len(a))
 export const setLen = (a: Vec2, length: number): Vec2 => mul(a, length / len(a))
 export const dot = ([x0, y0]: Vec2, [x1, y1]: Vec2): number => x0 * x1 + y0 * y1
